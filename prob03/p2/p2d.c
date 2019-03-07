7a3b0f497e2706c5f8d98cd035ff8e0fca7e0a81
Andre Rocha <andrefmrocha@live.com.pt>
2019-03-07 13:00:56 +0000

    Added some of the exercises of this week

---
+++ prob03/p2/p2d.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <sys/types.h> 
+#include <unistd.h> 
+
+int main(void) {
+    printf("1\n");
+    if(fork() > 0) {
+        printf("2");
+        printf("3");
+    } else {
+        printf("4");
+        printf("5");
+    }
+    printf("\n");
+    return 0;
+}
